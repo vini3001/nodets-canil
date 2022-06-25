e6434379ef4887dc176824ee78b60a7282f8e632
vini3001 <donizetevinicius250@gmail.com>
2022-06-25 15:25:28 -0300

    feito

--- scr/server.ts
+++ scr/server.ts
@@ -9,7 +9,7 @@ const server = express()
 server.set('view engine', 'mustache')
 server.set('views', path.join(__dirname, 'views'))
 server.engine('mustache', mustache())
-server.use(express.static(path.join(__dirname, './public')))
+server.use(express.static(path.join(__dirname, '../public')))
 server.use(express.urlencoded({extended:true}))
 server.use(mainPainel)
 
